98ba93179c7e126e5f44f8b3161b6a34281e35b6
sburakozdemir <sburakozdemir00@gmail.com>
2024-10-15 14:42:52 +0300

    Add project files.

---
+++ Pratik-Jwt/Data/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pratik_Jwt.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+    }
+}
